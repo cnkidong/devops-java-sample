6d855f3f13b6a815511582dff618167bbb77ec95
runzexia <runzexia@yunify.com>
2019-05-22 13:29:48 +0800

    Update Application.java

--- src/main/java/io/kubesphere/devops/Application.java
+++ src/main/java/io/kubesphere/devops/Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    
     public static void main(String[] args) {
+        String a;
         SpringApplication.run(Application.class,args);
     }
 }
